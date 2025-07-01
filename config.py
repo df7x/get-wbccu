9c8a2d4ee9decd268fa81f826ff36091daece18c
Server-WX <dongfang_qisu@163.com>
2025-07-01 16:51:39 +0800

    项目配置文件

---
+++ config.py
@@ -0,0 +1,14 @@
+title = "当前正在售卖的WBCCU"
+to_email = [
+    "用于接收邮件的邮箱"
+]
+# 发送邮件的时间，实例为每天0点0分5秒
+default_time = [0, 0, 5]
+from_email = "用于发送邮件的邮箱"
+login = from_email
+password = "发送邮箱的SMTP密码"
+smtp_server = "发送邮箱的SMTP服务器地址"
+# 发送邮箱的SMTP端口号
+smtp_port = 0
+
+body = "<!DOCTYPE html><html><head><style>*{padding:0;margin:0}img{width:100%}.div{display:grid;grid-template-columns:1fr 1.1fr 1.3fr;align-items: center;border:1px solid}.div:nth-child(even){background-color:#ccc}.div:nth-child(odd){background-color:#aaa}span{font-size:12px;font-family:微软雅黑}.name{padding-left:5px}</style></head><body>"
